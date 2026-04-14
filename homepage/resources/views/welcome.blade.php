fba07c6332667e2fef43eaa3adc1d4506038eb6c
Eril-Nakiri <122957034+Eril-Nakiri@users.noreply.github.com>
2026-04-14 13:49:10 +0700

    progress homepage
    baru homepage awal, laravel

---
+++ homepage/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+<h1>Halo Dunia, saya tidak lagi tersesat</h1>
+<title>Homepage</title>
+
+<div class="container mt-5 text-center">
+    <h1 class="text-primary">Project Laravel Saya</h1>
+    <p class="lead">Akhirnya tidak error lagi</p>
+    <button class="btn btn-success">Gaskeun</button>
+</div>
