ae15c3a75c08c02359fbb16f2b8768acc441d178
Eril-Nakiri <122957034+Eril-Nakiri@users.noreply.github.com>
2026-04-28 03:02:16 +0700

    perbaikan

--- homepage/resources/js/pages/AboutPage.jsx
+++ homepage/resources/js/pages/AboutPage.jsx
@@ -1,32 +1,14 @@
-import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
-export default function AboutPage() {
-    return (
-        <div className="page">
-        <Navbar />
-        <h1>About</h1>
-        <p>This is META portal project.</p>
-        </div>
-    );
+export default function AboutPage(){
+return(
+<>
+<Navbar/>
 
-    return (
-            <div>
-            <Navbar />
-
-            <div style={{ padding: "40px", color: "white" }}>
-                <h1>Dashboard</h1>
-
-                {!data ? (
-                <p>Connecting to Laravel...</p>
-                ) : (
-                <div>
-                    <h2>{data.app}</h2>
-                    <p>{data.status}</p>
-                    <p>Version: {data.version}</p>
-                </div>
-                )}
-            </div>
-            </div>
-        );
+<div className="page-content">
+<h1>About</h1>
+<p>This is META portal project.</p>
+</div>
+</>
+);
 }
