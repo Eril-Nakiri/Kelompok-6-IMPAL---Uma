1019e65408eb876c667e72593b5e7ac607083a62
Eril-Nakiri <122957034+Eril-Nakiri@users.noreply.github.com>
2026-05-22 20:21:09 +0700

    Update teams.sql

--- METAPORTAL SQL/teams.sql
+++ METAPORTAL SQL/teams.sql
@@ -1,23 +1,23 @@
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (1, 'Paper Rex', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (2, 'Rex Regum Qeon', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (3, 'Leviatán', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (4, 'Cloud9', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (5, 'Team Vitality', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (6, 'Bilibili Gaming', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (7, 'NRG', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (8, 'Trace Esports', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (9, 'XLG Esports', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (10, 'Team Liquid', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (11, 'T1', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (12, 'BBL Esports', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (13, 'Sentinels', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (14, 'Fnatic', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (15, 'Wolves Esports', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (16, 'EDward Gaming', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (17, 'FunPlus Phoenix', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (18, 'All Gamers', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (19, 'VARREL', null);
-INSERT INTO teams (id_team, nama_tim, logo_url) VALUES (20, 'Gen.G Esports', null);
+TRUNCATE TABLE teams RESTART IDENTITY CASCADE;
 
--- Menyelaraskan ulang urutan auto-increment ID untuk tabel teams
-SELECT setval(pg_get_serial_sequence('teams', 'id_team'), COALESCE(MAX(id_team), 1)) FROM teams;
+INSERT INTO teams (id_team, nama_tim, logo_url, singkatan) VALUES 
+(1, 'Paper Rex', 'https://owcdn.net/img/62bbebb185a7e.png', 'PRX'),
+(2, 'Rex Regum Qeon', 'https://owcdn.net/img/629f17f51e7a3.png', 'RRQ'),
+(3, 'Leviatán', 'https://owcdn.net/img/61b8888cc3860.png', 'LEV'),
+(4, 'Cloud9', 'https://owcdn.net/img/628addcbd509e.png', 'C9'),
+(5, 'Team Vitality', 'https://owcdn.net/img/6466d7936fd86.png', 'VIT'),
+(6, 'Bilibili Gaming', 'https://owcdn.net/img/63f25d72216c1.png', 'BLG'),
+(7, 'NRG', 'https://owcdn.net/img/6610f026c1a9e.png', 'NRG'),
+(8, 'Trace Esports', 'https://owcdn.net/img/6433a2d3b58c9.png', 'TE'),
+(9, 'XLG Esports', 'https://owcdn.net/img/671742f863b9b.png', 'XLG'),
+(10, 'Team Liquid', 'https://owcdn.net/img/640c38262824c.png', 'TL'),
+(11, 'T1', 'https://owcdn.net/img/62fe0b8f6b084.png', 'T1'),
+(12, 'BBL Esports', 'https://owcdn.net/img/65b8ccef5e273.png', 'BBL'),
+(13, 'Sentinels', 'https://owcdn.net/img/62875027c8e06.png', 'SEN'),
+(14, 'Fnatic', 'https://owcdn.net/img/62a40cc2b5e29.png', 'FNC'),
+(15, 'Wolves Esports', 'https://owcdn.net/img/651d33f8e6a1f.png', 'WOL'),
+(16, 'EDward Gaming', 'https://owcdn.net/img/62c8204fbb29d.png', 'EDG'),
+(17, 'FunPlus Phoenix', 'https://owcdn.net/img/65a17b313b131.png', 'FPX'),
+(18, 'All Gamers', 'https://owcdn.net/img/6549c2b905061.png', 'AG'),
+(19, 'VARREL', 'https://owcdn.net/img/63a746333bc0a.png', 'VRL'),
+(20, 'Gen.G Esports', 'https://owcdn.net/img/6970c986eddc6.png', 'GEN');
